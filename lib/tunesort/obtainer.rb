074a5a45c0be2fe329abc8122befc96a177f4462
Dominic Philip <domi.a.philip@gmail.com>
2017-01-01 16:24:13 -0500

    change 'get_tags' to be more precise with album_id

--- lib/tunesort/obtainer.rb
+++ lib/tunesort/obtainer.rb
@@ -1,15 +1,13 @@
+require 'json'
 require 'net/http'
 require 'taglib'
 
 module TuneSort
   class Obtainer
     def self.get_tags(album, artist, directory)
-      json_file_location = directory + '/itunes_tags.json'
-      unless File.exist?(json_file_location)
-        base_lookup_url = 'https://itunes.apple.com/search?term='
-        lookup = base_lookup_url + (artist + ' ' + album).downcase.tr!(' ', '+')
-        File.write(json_file_location, Net::HTTP.get(URI.parse(lookup)))
-      end
+      album_id = get_album_id(album, artist, directory)
+      lookup = "https://itunes.apple.com/lookup?id=#{album_id}&entity=song"
+      return Net::HTTP.get(URI.parse(lookup))
     end
 
     def self.get_track_number(song)
@@ -17,5 +15,21 @@ module TuneSort
         return tune.tag.track unless tune.nil?
       end
     end
+
+    def self.get_album_id(album, artist, directory)
+      lookup = 'https://itunes.apple.com/search?term='
+      query = (artist + ' ' + album).downcase.tr!(' ', '+') + '&entity=album'
+      album_ids_hash = JSON.parse(Net::HTTP.get(URI.parse(lookup + query)))
+      unless album_ids_hash['resultCount'].to_i.zero?
+        (0..tags_hash['resultCount'].to_i).each do |key|
+          itunes_rating = tags_hash.dig('results', key, 'contentAdvisoryRating')
+          unless itunes_rating == 'clean'
+            return tags_hash.dig('results', key, 'collectionId')
+          end
+        end
+      end
+    end
+
+    private_class_method :self.get_album_id
   end
 end
--- lib/tunesort/util.rb
+++ lib/tunesort/util.rb
@@ -16,34 +16,7 @@ module TuneSort
     end
 
     def self.parse(directory, track_number)
-      if File.exist?(directory + '/itunes_tags.json')
-        tags_hash = JSON.parse(File.read(directory + '/itunes_tags.json'))
-        unless tags_hash['resultCount'].to_i.zero?
-          (0..tags_hash['resultCount'].to_i).each do |key|
-            itunes_track_number = tags_hash.dig('results', key, 'trackNumber').to_i
-            unless track_number.nil?
-              if track_number.eql? itunes_track_number
-                itunes_rating = tags_hash.dig('results', key, 'trackExplicitness')
-                if itunes_rating == 'explicit'
-                  info = []
-                  (0..ITUNES_TAG_KEYS.length).each do |index|
-                    info.push(tags_hash.dig('results', key, ITUNES_TAG_KEYS[index]))
-                  end
-                  return info
-                elsif itunes_rating == 'notExplicit'
-                  info = []
-                  (0..ITUNES_TAG_KEYS.length).each do |index|
-                    info.push(tags_hash.dig('results', key, ITUNES_TAG_KEYS[index]))
-                  end
-                  return info
-                end
-              end
-            end
-          end
-        end
-      else
-        raise 'tunesort: error: iTunes tags file has not been downloaded'
-      end
+      # TODO - Redo the method according to changes in obtainer.
     end
   end
 end
